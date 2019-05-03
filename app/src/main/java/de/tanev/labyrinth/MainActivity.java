abd86002c5d5b49357ff6c2405c49509752914aa
Admin <doxmoxbox@gmail.com>
2019-05-03 20:44:18 +0200

    Initial.

---
+++ app/src/main/java/de/tanev/labyrinth/MainActivity.java
@@ -0,0 +1,15 @@
+package de.tanev.labyrinth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
