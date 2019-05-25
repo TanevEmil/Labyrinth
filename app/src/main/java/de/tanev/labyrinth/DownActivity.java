a4146ade75aca01e2bcef926a0c27c882d5d298b
Admin <doxmoxbox@gmail.com>
2019-05-25 17:02:50 +0200

    3 activities. Init state is now Java coded instead of XML.

--- app/src/main/java/de/tanev/labyrinth/DownActivity.java
+++ app/src/main/java/de/tanev/labyrinth/DownActivity.java
@@ -6,87 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-import java.sql.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
+public class DownActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity {
-    BinarySearch bs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        bs = new BinarySearch();
-        int findMe = bs.getFindMe();
-
-        int indexMin = 0;
-        int indexMax = bs.getArray().length - 1;
-        int indexLowAvg = (bs.getArray().length - 1) / 2;
-        int indexHighAvg = (bs.getArray().length - 1) / 2 + 1;
-        int index1div4 = indexMax / 4;
-        int index3div4 = 3 * indexMax / 4;
-
-        int intMin = bs.getArray()[indexMin];
-        int intMax = bs.getArray()[indexMax];
-        int intLowAvg = bs.getArray()[indexLowAvg];
-        int intHighAvg = bs.getArray()[indexHighAvg];
-        final int int1div4 = bs.getArray()[index1div4];
-        final int int3div4 = bs.getArray()[index3div4];
-
-        String sButtonLP2 = "^^\n[ " + indexLowAvg + " ]";
-        String sButtonLP1 = "^\n" + intLowAvg;
-        String sButtonL = "<\n[ " + index1div4 + " ]";
-        String sButtonLM1 = "v\n" + intMin;
-        String sButtonLM2 = "<\n[ " + indexMin + " ]";
-
-        String sButtonP1 = "^^\n[ " + indexMax + " ]";
-        String sButtonC = "^\n" + intMax;
-        final String sButtonM1 = "!=\n" + findMe;
-        String sButtonM2 = "v\n" + intMin;
-
-        String sButtonRP2 = "^^\n[ " + indexMax + " ]";
-        String sButtonRP1 = "^\n" + intMax;
-        String sButtonR = ">\n[ " + index3div4 + " ]";
-        String sButtonRM1 = "v\n" + intHighAvg;
-        String sButtonRM2 = "vv\n[ " + indexHighAvg + " ]";
-
-        final Bundle extras = new Bundle();
-
-        extras.putString("ButtonLP2", sButtonLP2);
-        extras.putString("ButtonLP1", sButtonLP1);
-//        extras.putString("ButtonL", sButtonL);
-        extras.putString("ButtonLM1", sButtonLM1);
-        extras.putString("ButtonLM2", sButtonLM2);
-
-        extras.putString("ButtonP1", sButtonP1);
-        extras.putString("ButtonC", sButtonC);
-        extras.putString("ButtonM1", sButtonM1);
-        extras.putString("ButtonM2", sButtonM2);
-
-        extras.putString("ButtonRP2", sButtonRP2);
-        extras.putString("ButtonRP1", sButtonRP1);
-//        extras.putString("ButtonR", sButtonR);
-        extras.putString("ButtonRM1", sButtonRM1);
-        extras.putString("ButtonRM2", sButtonRM2);
-
-        ((Button) findViewById(R.id.buttonLP2)).setText(sButtonLP2);
-        ((Button) findViewById(R.id.buttonLP1)).setText(sButtonLP1);
-        ((Button) findViewById(R.id.buttonL)).setText(sButtonL);
-        ((Button) findViewById(R.id.buttonLM1)).setText(sButtonLM1);
-        ((Button) findViewById(R.id.buttonLM2)).setText(sButtonLM2);
-
-        ((Button) findViewById(R.id.buttonP1)).setText(sButtonP1);
-        ((Button) findViewById(R.id.buttonC)).setText(sButtonC);
-        ((Button) findViewById(R.id.buttonM1)).setText(sButtonM1);
-        ((Button) findViewById(R.id.buttonM2)).setText(sButtonM2);
-
-        ((Button) findViewById(R.id.buttonRP2)).setText(sButtonRP2);
-        ((Button) findViewById(R.id.buttonRP1)).setText(sButtonRP1);
-        ((Button) findViewById(R.id.buttonR)).setText(sButtonR);
-        ((Button) findViewById(R.id.buttonRM1)).setText(sButtonRM1);
-        ((Button) findViewById(R.id.buttonRM2)).setText(sButtonRM2);
-
-
+        setContentView(R.layout.down_activity);
 
 //        final Button buttonLP2 = findViewById(R.id.buttonLP2);
 //        final Button buttonP2 = findViewById(R.id.buttonP2);
@@ -159,10 +84,11 @@ public class MainActivity extends AppCompatActivity {
         buttonL.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent activityChangeIntent =
-                        new Intent(MainActivity.this, RainbowActivity.class);
-                extras.putString("ButtonL","<<\n" + int1div4);
-                extras.putString("ButtonR",sButtonM1);
-                extras.putIntegerArrayList("BS", new ArrayList(Arrays.asList(bs.getArray())));
+                        new Intent(DownActivity.this, RainbowActivity.class);
+                Bundle extras = new Bundle();
+                extras.putString("ButtonL","<<");
+                extras.putString("ButtonC","<");
+                extras.putString("ButtonR","?");
                 activityChangeIntent.putExtras(extras);
                 startActivity(activityChangeIntent);
             }
@@ -179,10 +105,11 @@ public class MainActivity extends AppCompatActivity {
         buttonR.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent activityChangeIntent =
-                        new Intent(MainActivity.this, RainbowActivity.class);
-                extras.putString("ButtonL",sButtonM1);
-                extras.putString("ButtonR",">>\n" + int3div4);
-                extras.putIntegerArrayList("BS", new ArrayList(Arrays.asList(bs.getArray())));
+                        new Intent(DownActivity.this, RainbowActivity.class);
+                Bundle extras = new Bundle();
+                extras.putString("ButtonL","?");
+                extras.putString("ButtonC",">");
+                extras.putString("ButtonR",">>");
                 activityChangeIntent.putExtras(extras);
                 startActivity(activityChangeIntent);
             }
--- app/src/main/java/de/tanev/labyrinth/RainbowActivity.java
+++ app/src/main/java/de/tanev/labyrinth/RainbowActivity.java
@@ -13,36 +13,157 @@ public class RainbowActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.rainbow_activity);
 
+        Bundle extras = getIntent().getExtras();
+        final String l = extras.getString("ButtonL");
+//        final String c = extras.getString("ButtonC");
+        final String r = extras.getString("ButtonR");
+
+        ((Button) findViewById(R.id._buttonL)).setText(l);
+//        ((Button) findViewById(R.id._buttonC)).setText(c);
+        ((Button) findViewById(R.id._buttonR)).setText(r);
+
 //        final Button _buttonLP2 = findViewById(R.id._buttonLP2);
 //        final Button _buttonP2 = findViewById(R.id._buttonP2);
-//        final Button _buttonPR2 = findViewById(R.id._buttonRP2);
+//        final Button _buttonRP2 = findViewById(R.id._buttonRP2);
 //
 //        final Button _buttonLP1 = findViewById(R.id._buttonLP1);
-//        final Button _buttonP1 = findViewById(R.id._buttonP1);
+        final Button _buttonP1 = findViewById(R.id._buttonP1);
 //        final Button _buttonRP1 = findViewById(R.id._buttonRP1);
-//
-//        final Button _buttonL = findViewById(R.id._buttonL);
-        final Button _buttonC = findViewById(R.id._buttonC);
-//        final Button _buttonR = findViewById(R.id._buttonR);
-//
+
+        final Button _buttonL = findViewById(R.id._buttonL);
+//        final Button _buttonC = findViewById(R.id._buttonC);
+        final Button _buttonR = findViewById(R.id._buttonR);
+
 //        final Button _buttonLM1 = findViewById(R.id._buttonLM1);
-//        final Button _buttonM1 = findViewById(R.id._buttonM1);
+        final Button _buttonM1 = findViewById(R.id._buttonM1);
 //        final Button _buttonRM1 = findViewById(R.id._buttonRM1);
 //
 //        final Button _buttonLM2 = findViewById(R.id._buttonLM2);
 //        final Button _buttonM2 = findViewById(R.id._buttonM2);
 //        final Button _buttonRM2 = findViewById(R.id._buttonRM2);
 
-        _buttonC.setOnClickListener(new View.OnClickListener() {
+//        _buttonLP2.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                Intent activityChangeIntent =
+//                        new Intent(RainbowActivity.this, MainActivity.class);
+//                RainbowActivity.this.startActivity(activityChangeIntent);
+//            }
+//        });
+//
+//        _buttonP2.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                Intent activityChangeIntent =
+//                        new Intent(RainbowActivity.this, MainActivity.class);
+//                RainbowActivity.this.startActivity(activityChangeIntent);
+//            }
+//        });
+//
+//        _buttonRP2.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                Intent activityChangeIntent =
+//                        new Intent(RainbowActivity.this, MainActivity.class);
+//                RainbowActivity.this.startActivity(activityChangeIntent);
+//            }
+//        });
+//
+//        _buttonLP1.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                Intent activityChangeIntent =
+//                        new Intent(RainbowActivity.this, MainActivity.class);
+//                RainbowActivity.this.startActivity(activityChangeIntent);
+//            }
+//        });
+        _buttonP1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Perform action on click
-                Intent activityChangeIntent = new Intent(RainbowActivity.this, MainActivity.class);
-
-                // currentContext.startActivity(activityChangeIntent);
-
+                Intent activityChangeIntent =
+                        new Intent(RainbowActivity.this, MainActivity.class);
                 RainbowActivity.this.startActivity(activityChangeIntent);
             }
         });
+
+//        _buttonRP1.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                Intent activityChangeIntent =
+//                        new Intent(RainbowActivity.this, MainActivity.class);
+//                RainbowActivity.this.startActivity(activityChangeIntent);
+//            }
+//        });
+
+        _buttonL.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+//                if(l.equals("!=")) {
+                    Intent activityChangeIntent =
+                            new Intent(RainbowActivity.this, MainActivity.class);
+                    RainbowActivity.this.startActivity(activityChangeIntent);
+                }
+//            }
+        });
+
+//        _buttonC.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                Intent activityChangeIntent =
+//                        new Intent(RainbowActivity.this, MainActivity.class);
+//                RainbowActivity.this.startActivity(activityChangeIntent);
+//            }
+//        });
+
+        _buttonR.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+//                if (r.equals("!=")) {
+                    Intent activityChangeIntent =
+                            new Intent(RainbowActivity.this, MainActivity.class);
+                    RainbowActivity.this.startActivity(activityChangeIntent);
+                }
+//            }
+        });
+
+//        _buttonLM1.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                Intent activityChangeIntent =
+//                        new Intent(RainbowActivity.this, MainActivity.class);
+//                RainbowActivity.this.startActivity(activityChangeIntent);
+//            }
+//        });
+
+        _buttonM1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent activityChangeIntent =
+                        new Intent(RainbowActivity.this, DownActivity.class);
+                RainbowActivity.this.startActivity(activityChangeIntent);
+            }
+        });
+
+//        _buttonRM1.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                Intent activityChangeIntent =
+//                        new Intent(RainbowActivity.this, MainActivity.class);
+//                RainbowActivity.this.startActivity(activityChangeIntent);
+//            }
+//        });
+//
+//        _buttonLM2.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                Intent activityChangeIntent =
+//                        new Intent(RainbowActivity.this, MainActivity.class);
+//                RainbowActivity.this.startActivity(activityChangeIntent);
+//            }
+//        });
+//
+//        _buttonM2.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                Intent activityChangeIntent =
+//                        new Intent(RainbowActivity.this, MainActivity.class);
+//                RainbowActivity.this.startActivity(activityChangeIntent);
+//            }
+//        });
+//
+//        _buttonRM2.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                Intent activityChangeIntent =
+//                        new Intent(RainbowActivity.this, MainActivity.class);
+//                RainbowActivity.this.startActivity(activityChangeIntent);
+//            }
+//        });
     }
 }
 
